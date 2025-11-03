d844a91d17c7c75d240040f2e8c1f5802adc1823
javirerffggg <160790690+javirerffggg@users.noreply.github.com>
2025-11-03 16:58:43 +0100

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-restricted-globals */
+
+// Este service worker puede ser personalizado
+// Para una PWA básica, este código almacena en caché los recursos estáticos
+
+const CACHE_NAME = 'coach-definicion-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/bundle.js',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Abriendo caché');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
